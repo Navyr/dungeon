63d85650547c481017c4446319a187b25fa59ec0
megane22 <megan.elmore@girlswhocode.com>
2017-07-24 19:44:19 -0700

    Starter code - sample dungeon layout

---
+++ dungeon-layout.py
@@ -0,0 +1,8 @@
+dungeon = [['Rm 101', 'Rm 102', 'Rm 103', 'Rm 104', 'Elevator'],
+           ['Rm 201', 'Rm 202 (my room)', 'Rm 203', 'Rm 204', 'Elevator'],
+           ['Rm 301', 'secret stairs', 'Rm 303', 'Rm 304', 'Elevator'],
+           ['Rm 401', 'secret stairs', 'maintenance closet', 'Rm 404', 'dead end']]
+
+floor_number = 0
+room_number = 3
+print(dungeon[floor_number][room_number])
